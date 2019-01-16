65964300338b4f79c0031fa27148ebd1376d1ad0
sub-h <42210957+sub-h@users.noreply.github.com>
2019-01-16 17:24:35 +0530

    Create add1.c

---
+++ add1.c
@@ -0,0 +1,11 @@
+#include<iostream.h>
+void add1(){
+  int num;
+  printf("Enter the number in which you want to add 1 to\n ");
+  scanf("%d",&num);
+  int out=num+1;
+  printf("The result is %d",out);
+}
+void main(){
+  add1();
+}
