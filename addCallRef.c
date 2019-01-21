905a49428f11823811091bb19dfd31dd543e3118
sub-h <42210957+sub-h@users.noreply.github.com>
2019-01-21 18:04:13 +0530

    Update addCallRef.c

--- addCallRef.c
+++ addCallRef.c
@@ -1,3 +1,21 @@
-#include<iostream.h>
-void add(int *a,int *b){
-  
+#include <stdio.h>
+#include <stdlib.h>
+void input(int *a,int *b)
+{
+    printf("Enter the first number\n");
+    scanf("%d",&a);
+    printf("Enter the second number\n");
+    scanf("%d",&b);
+}
+void compute(int a,int b,int c){
+    c=a+b;
+}
+void output(){
+    int a,b,c;
+    printf("%d+%d=%d",a,b,c);
+}
+void main(){
+    input(&a,&b);
+    
+    
+}
