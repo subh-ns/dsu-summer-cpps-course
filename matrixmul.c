855fa6eb7b534bf9cd711ac865b6b21f8fa0465c
sub-h <42210957+sub-h@users.noreply.github.com>
2019-03-22 15:49:33 +0530

    Update matrixmul.c

--- matrixmul.c
+++ matrixmul.c
@@ -10,11 +10,7 @@ int main()
         int b[r2][c2];
         int product[r1][c2];
         for(i=0;i<r1;i++)
-        {}
-        else
-                printf("Matrix multiplication not possible\n");
-}
-
+        {
                 for(j=0;j<c2;j++)
                 {
                         product[i][j]=0;
@@ -25,7 +21,7 @@ int main()
                 printf("Matrix multiplication is possible\n");
                 printf("Enter the element of the first matrix\n");
                 for(i=0;i<r1;i++)
-{
+                {
                         for(j=0;j<c1;j++)
                         {
                                 printf("Enter data in [%d] [%d] position: ",i,j);
@@ -39,31 +35,28 @@ int main()
                         {
                                 printf("Enter data in [%d] [%d] position: ",i,j);
                                 scanf("%d",&b[i][j]);
-                        }for(i=0;i<r1;i++)
-                {
-                        for(j=0;j<c2;j++)
+                        }
+                        for(i=0;i<r1;i++)
                         {
-                   }
-        else
-                printf("Matrix multiplication not possible\n");
-}
-             for(k=0;k<c1;k++)
+                                for(j=0;j<c2;j++)
                                 {
-                                        product[i][j]+=a[i][k]*b[k][j];
+                                        for(k=0;k<c1;k++)
+                                        {
+                                                product[i][j]+=a[i][k]*b[k][j];
+                                        }
                                 }
                         }
+                        printf("Order of resultant matrix is %dx%d\n",r1,c2);
+                        printf("Resultant Matrix is\n");
+                                for(i=0;i<r1;i++)
+                                {
+                                        for(j=0;j<c2;j++)
+                                        {
+                                                printf("%d\t",product[i][j]);
+                                        }
+                                printf("\n");
+                                }
                 }
-                printf("Order of resultant matrix is %dx%d\n",r1,c2);
-                printf("Resultant Matrix is\n");
-                for(i=0;i<r1;i++)
-                {
-                        for(j=0;j<c2;j++)
-                        {
-                                printf("%d\t",product[i][j]);
-                        }
-                        printf("\n");
-                }
-        }
         else
                 printf("Matrix multiplication not possible\n");
 }
